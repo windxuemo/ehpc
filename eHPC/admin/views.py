06da80ae1a17da3c8728242e052c9df269b198a9
wzl281158150 <281158150@qq.com>
2016-10-21 07:46:17 -0700

    增加了管理员页面选择题添加的功能

--- eHPC/admin/views.py
+++ eHPC/admin/views.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from flask import render_template, request, redirect, url_for
 from . import admin
-from ..models import Course
+from ..models import Course, Classify, Choice
 from ..user.authorize import admin_login
 from .. import db
 import os
@@ -44,7 +44,26 @@ def lesson():
 
 
 @admin.route('/problem')
-def problem(): pass
+def problem():
+    choice = Choice.query.all()
+    return render_template('admin/problem/choice.html', choices=choice)
+
+
+@admin.route('/problem/choice_problem/')
+def choice_problem():
+    classifies = Classify.query.all()
+    if 'id' in request.args:
+        choice = Choice.query.filter_by(id=request.args['id']).first_or_404()
+        classify_selected = ""
+        for temp in choice.classifies:
+            classify_selected += unicode(temp.id) + u';'
+        classify_selected = classify_selected[:len(classify_selected) - 1]
+
+        options = choice.detail.split(';')
+        return render_template('admin/problem/edit-choice-problem.html', choice=choice,
+                               classifies=classifies, classify_selected=classify_selected, options=options)
+    else:
+        return render_template('admin/problem/add-new-choice-problem.html', classifies=classifies)
 
 
 @admin.route('/group')
@@ -85,6 +104,51 @@ def process():
         db.session.add(c)
         db.session.commit()
         return unicode(c.id)
+    elif request.form['op'] == 'create_choice_problem':
+        if request.form['c_type'] == '0':
+            c_type = False
+        else:
+            c_type = True
+        choice = Choice(title=request.form['title'], detail=request.form['detail'],
+                        c_type=c_type, answer=request.form['answer'])
+
+        classify = request.form.getlist('classify[]')
+
+        for i in classify:
+            classify = Classify.query.filter_by(id=i).first_or_404()
+            choice.classifies.append(classify)
+
+        db.session.add(choice)
+        db.session.commit()
+        return unicode(choice.id)
+    elif request.form['op'] == 'edit_choice_problem':
+        choice = Choice.query.filter_by(id=request.form['id']).first_or_404()
+        choice.title = request.form['title']
+        choice.detail = request.form['detail']
+        if request.form['c_type'] == '0':
+            c_type = False
+        else:
+            c_type = True
+        choice.c_type = c_type
+        choice.answer = request.form['answer']
+
+        classify = request.form.getlist('classify[]')
+
+        for i in choice.classifies:
+            choice.classifies.remove(i)
+
+        for i in classify:
+            classify = Classify.query.filter_by(id=i).first_or_404()
+            choice.classifies.append(classify)
+
+        db.session.commit()
+        return unicode(choice.id)
+    elif request.form['op'] == "del_choice":
+        choice = Choice.query.filter_by(id=request.form['id']).first_or_404()
+        db.session.delete(choice)
+        db.session.commit()
+
+        return unicode(choice.id)
     else:
         return "error"
 
@@ -94,7 +158,7 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'bmp'}
 
 def allowed_file(filename):
     return '.' in filename and \
-        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
 @admin.route('/upload', methods=['POST'])
--- eHPC/util/problem_choices.py
+++ eHPC/util/problem_choices.py
@@ -4,3 +4,8 @@ from . import filter_blueprint
 @filter_blueprint.app_template_filter('split')
 def split(string, c, n):
     return string.split(c)[n]
+
+
+@filter_blueprint.app_template_filter('get_char')
+def get_char(n):
+    return chr(n+ord('A'))

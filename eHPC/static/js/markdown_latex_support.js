2ac422afcf3fa1d6498f13c6345a29ad281d8666
wzl281158150 <281158150@qq.com>
2017-01-09 14:14:10 +0800

    修复作业页面不渲染问题 (#270)

--- eHPC/static/js/markdown_latex_support.js
+++ eHPC/static/js/markdown_latex_support.js
@@ -5,14 +5,14 @@ function latex_support(plainText) {
         for(var i=0;i<temp.length;i++){
             $("#latex-render-area").append("<span id='sp-rep-latex-" + (i+1).toString() + "'>" + temp[i] + "</span>");
         }
-        MathJax.Hub.Queue(["Typeset", MathJax.Hub, 'latex-render-area']);
         var count = 0;
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub, 'latex-render-area']);
+        count = 0;
         plainText = plainText.replace(/\\\([\s\S]*?\\\)|\\\[[\s\S]*?\\\]/g, function (word) {
             count++;
             return "sp-rep-latex-" + count.toString();
         });
         plainText = SimpleMDE.prototype.markdown(plainText);
-
         count = 0;
         plainText = plainText.replace(/sp-rep-latex-\d*/g, function (word) {
             count++;

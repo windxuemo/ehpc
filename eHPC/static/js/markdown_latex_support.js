cd8186a80a15fba689869ba9c12a80eb8f1ac910
wzl281158150 <281158150@qq.com>
2017-01-09 11:33:49 +0800

    添加latex行内公式支持 (#266)

--- eHPC/static/js/markdown_latex_support.js
+++ eHPC/static/js/markdown_latex_support.js
@@ -1,13 +1,13 @@
 document.write('<script type="text/javascript" src="https://cdn.bootcss.com/mathjax/2.7.0/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>');
 function latex_support(plainText) {
-    var temp = plainText.match(/\\\[[\s\S]*?\\\]/g);
+    var temp = plainText.match(/\\\([\s\S]*?\\\)|\\\[[\s\S]*?\\\]/g);
     if(temp!=null){
         for(var i=0;i<temp.length;i++){
             $("#latex-render-area").append("<span id='sp-rep-latex-" + (i+1).toString() + "'>" + temp[i] + "</span>");
         }
         MathJax.Hub.Queue(["Typeset", MathJax.Hub, 'latex-render-area']);
         var count = 0;
-        plainText = plainText.replace(/\\\[[\s\S]*?\\\]/g, function (word) {
+        plainText = plainText.replace(/\\\([\s\S]*?\\\)|\\\[[\s\S]*?\\\]/g, function (word) {
             count++;
             return "sp-rep-latex-" + count.toString();
         });
@@ -19,8 +19,11 @@ function latex_support(plainText) {
             return $("#sp-rep-latex-" + count.toString()).html();
         });
         $("#latex-render-area").empty();
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
     }
-    MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+    else{
+        plainText = SimpleMDE.prototype.markdown(plainText);
+    }
     return plainText;
 }
 

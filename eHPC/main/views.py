69086969e48b2f9a307adcef0a7d9413af6232cb
xuelangZF <xuezaigds@gmail.com>
2017-02-18 19:32:25 +0800

    更改滚动图和Logo

--- eHPC/main/views.py
+++ eHPC/main/views.py
@@ -1,6 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: xuezaigds@gmail.com
+
 from flask import render_template
 from . import main
-from ..models import Course, Group, Article, User, Post, UserQuestion, SubmitProblem, Progress, Knowledge
+from ..models import Course, Group, Article, User, Post, UserQuestion, SubmitProblem, Progress
 from flask_babel import gettext
 from flask_wtf.csrf import CSRFError
 from flask_login import current_user
@@ -8,50 +12,51 @@ from flask_login import current_user
 
 @main.route('/')
 def index():
-        courses = Course.query.limit(8)
-        groups = Group.query.order_by(Group.memberNum.desc()).limit(6)
-        articles = Article.query.order_by(Article.updatedTime.desc()).limit(5)
-        if current_user.is_authenticated:
-            user_posts = current_user.posts.order_by(Post.createdTime.desc()).limit(8).all()
-            user_topics = []
-            for p in user_posts:
-                user_topics.append(p.topic)
-            user_courses = current_user.courses
-            user_question = UserQuestion.query.order_by(UserQuestion.last_time.desc()).limit(5).all()
-            user_submits = SubmitProblem.query.order_by(SubmitProblem.submit_time.desc()).filter_by(uid=current_user.id).limit(5).all()
-            user_program = []
-            for s in user_submits:
-                user_program.append(s.program)
-            all_challenges = Progress.query.order_by(Progress.update_time.desc()).filter_by(user_id=current_user.id).all()
-            user_challenges = []
-            count = 0
-            for ac in all_challenges:
-                if count > 5:
-                    break
-                challenges_count = ac.knowledge.challenges.count()
-                if ac.cur_progress < challenges_count:
-                    user_challenges.append(ac)
-                    count += 1
-            if user_courses or user_topics or user_question or user_program or user_challenges:
-                return render_template('main/personal_index.html',
-                                       title=gettext('Education Practice Platform'),
-                                       user_courses=user_courses,
-                                       user_topics=user_topics,
-                                       user_question=user_question,
-                                       user_program=user_program,
-                                       user_challenges=user_challenges)
-            else:
-                return render_template('main/index.html',
-                                       title=gettext('Education Practice Platform'),
-                                       courses=courses,
-                                       groups=groups,
-                                       articles=articles)
+    courses = Course.query.limit(8)
+    groups = Group.query.order_by(Group.memberNum.desc()).limit(6)
+    articles = Article.query.order_by(Article.updatedTime.desc()).limit(5)
+    if current_user.is_authenticated:
+        user_posts = current_user.posts.order_by(Post.createdTime.desc()).limit(8).all()
+        user_topics = []
+        for p in user_posts:
+            user_topics.append(p.topic)
+        user_courses = current_user.courses
+        user_question = UserQuestion.query.order_by(UserQuestion.last_time.desc()).limit(5).all()
+        user_submits = SubmitProblem.query.order_by(SubmitProblem.submit_time.desc()).filter_by(
+            uid=current_user.id).limit(5).all()
+        user_program = []
+        for s in user_submits:
+            user_program.append(s.program)
+        all_challenges = Progress.query.order_by(Progress.update_time.desc()).filter_by(user_id=current_user.id).all()
+        user_challenges = []
+        count = 0
+        for ac in all_challenges:
+            if count > 5:
+                break
+            challenges_count = ac.knowledge.challenges.count()
+            if ac.cur_progress < challenges_count:
+                user_challenges.append(ac)
+                count += 1
+        if user_courses or user_topics or user_question or user_program or user_challenges:
+            return render_template('main/personal_index.html',
+                                   title=gettext('Education Practice Platform'),
+                                   user_courses=user_courses,
+                                   user_topics=user_topics,
+                                   user_question=user_question,
+                                   user_program=user_program,
+                                   user_challenges=user_challenges)
         else:
             return render_template('main/index.html',
                                    title=gettext('Education Practice Platform'),
                                    courses=courses,
                                    groups=groups,
                                    articles=articles)
+    else:
+        return render_template('main/index.html',
+                               title=gettext('Education Practice Platform'),
+                               courses=courses,
+                               groups=groups,
+                               articles=articles)
 
 
 @main.app_errorhandler(404)
